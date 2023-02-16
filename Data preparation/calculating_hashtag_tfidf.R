8f5f0b16738681ca43816d104773df6ecba7784e
lrowleyabel <lrowleyabel@gmail.com>
2023-02-16 13:38:24 +0000

    Updated hashtag TF-IDF calculation

--- Data preparation/calculating_hashtag_tfidf.R
+++ Data preparation/calculating_hashtag_tfidf.R
@@ -6,7 +6,7 @@
 
 
 # LAURENCE ROWLEY-ABEL, UNIVERSITY OF EDINBURGH
-# UPDATED: 08/01/23
+# UPDATED: 16/02/23
 
 # DESCRIPTION: This file takes the hashtags used by each candidate and creates a score representing how
 # distinctive their hashtags are (the TF-IDF score) based on each hashtag's frequency in their Tweets compared
@@ -19,6 +19,7 @@ library(tidytext)
 library(ggplot2)
 library(utils)
 library(readxl)
+library(lubridate)
 
 setwd(choose.dir(caption = "Select working directory"))
 rm(list = ls())
@@ -27,16 +28,24 @@ rm(list = ls())
 ### STEP 1: SET UP DATA IN TIDY TEXT FORMAT ###
 
 
+# Get directory containing processed data
+dataset_dir<- choose.dir("Select directory containing metrics data on University of Nottingham OneDrive, under Twitter Scrapes > Datasets")
+
 # Load in hashtag data
-load("Hashtag Data.Rda")
+load(paste0(dataset_dir, "//Hashtag Data/Rda Formatted Data//Hashtag Data.Rda"))
+
+# Parse date as a date
+hashtag_df<- hashtag_df%>%
+  mutate(date = parse_date_time(tweet_date, "a b d H:M:S z Y")%>%
+           lubridate::date())
+
+# Filter for only hashtags that appear in tweets posted within the 9 months prior to the election
+hashtag_df<- hashtag_df%>%
+  filter(date >= dmy("12/09/2021"))
 
 # Select relevant variables
 hashtag_df<- hashtag_df%>%
   select(BIOID, hashtags)
-
-# Tokenise the hashtags (ie: create a dataframe with one row for each BIOID-hashtag combination that occurs)
-hashtag_df<- hashtag_df%>%
-  unnest_tokens
 
 
 ### STEP 2: CALCULATE MEAN OVERALL TF-IDF FOR EACH CANDIDATE ###
@@ -46,19 +55,11 @@ hashtag_df<- hashtag_df%>%
 
 # Calculate hashtag frequency for each BIOID
 htag_freq<- hashtag_df%>%
-  count(BIOID, hashtag, sort = T)
-
-# Calculate overall hashtag frequency
-total_htags<- htag_freq%>%
-  group_by(BIOID)%>%
-  summarise(total_htags = sum(n))
-
-# Join total hashtag frequency to the dataframe of frequency per BIOID
-htag_freq<- left_join(htag_freq, total_htags)
+  count(BIOID, hashtags, sort = T)
 
 # Calculate the overall TF-IDF score for each BIOID-hashtag combination
 tfidf<- htag_freq%>%
-  bind_tf_idf(hashtag, BIOID, n)
+  bind_tf_idf(hashtags, BIOID, n)
 
 # Calculate the mean overall TF-IDF score for each BIOID
 mean_tfidf<- tfidf%>%
@@ -76,10 +77,10 @@ ggplot(mean_tfidf)+
 # This step calculates how distinct, on average, each candidate's tweets were compared to the tweets of others from their party
 
 # Get directory containing main candidate datasets on University of Nottingham OneDrive
-data_dir<- choose.dir(caption = "Select directory containing main candidate datasets (TplF - Working Datasets)")
+candidate_dataset_dir<- choose.dir(caption = "Select directory containing main candidate datasets (TplF - Working Datasets)")
 
 # Read in candidate dataset
-candidate_df<- read_excel(paste0(data_dir, "//Substantive Data Combined Candidates 05-01-23.xlsx"))
+candidate_df<- read_excel(paste0(candidate_dataset_dir, "//Substantive Data Combined Candidates 05-01-23.xlsx"))
 
 # Select the variables indicating party/list name and whether the candidate is independent from the candidate dataset
 candidate_df<- candidate_df%>%
@@ -108,7 +109,7 @@ for (party in unique(htag_freq$party_list)){
   
   out_df<- htag_freq%>%
     filter(party_list == party)%>%
-    bind_tf_idf(hashtag, BIOID, n)
+    bind_tf_idf(hashtags, BIOID, n)
   
   if(exists("party_tfidf")){
     party_tfidf<- rbind(party_tfidf, out_df)
@@ -132,15 +133,14 @@ mean_party_tfidf<- party_tfidf%>%
 mean_tfidf<- left_join(mean_tfidf, mean_party_tfidf, by = "BIOID")
 
 # Save the dataframe of mean TF-IDF scores
-save(mean_tfidf, file = "Mean TFIDF Scores.Rda")
+save(mean_tfidf, file = paste0(dataset_dir, "//Hashtag Data//Rda Formatted Data//Mean Hashtag TFIDF Scores.Rda"))
 
 # Get the existing overall metrics data
-data_dir<- choose.dir("Select data directory on University of Nottingham OneDrive under Twitter Scrapes > Datasets")
-load(paste0(data_dir, "\\Overall Metrics\\Overall Twitter Metrics.Rda"))
+load(paste0(dataset_dir, "\\Overall Metrics\\Overall Twitter Metrics.Rda"))
 
 # Join the mean TF-IDF score to the existing metrics data
 metrics_df<- left_join(metrics_df, mean_tfidf, by = "BIOID")
 
 # Save the updated metrics data as Rda and CSV file
-save(metrics_df, file = paste0(data_dir, "\\Overall Metrics\\Overall Twitter Metrics.Rda"))
-write.csv(metrics_df, file = paste0(data_dir, "\\Overall Metrics\\Overall Twitter Metrics.csv"), row.names = F, fileEncoding = "UTF-8")
+save(metrics_df, file = paste0(dataset_dir, "\\Overall Metrics\\Overall Twitter Metrics.Rda"))
+write.csv(metrics_df, file = paste0(dataset_dir, "\\Overall Metrics\\Overall Twitter Metrics.csv"), row.names = F, fileEncoding = "UTF-8")
